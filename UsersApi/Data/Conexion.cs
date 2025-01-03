1cb943861321cdca72c46f58baa2fc6c46fc47b0
Siberia17 <sgdn10@gmail.com>
2025-01-03 09:47:49 -0500

    Register user and login

---
+++ UsersApi/Data/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace UsersApi.Data
+{
+    public class Conexion
+    {
+        public static SqlConnection GetConexion()
+        {
+            string connectionString = "Data Source=DESKTOP-K8DNTUE\\MSSQLSERVER2;Database=SiberiaPets;Persist Security Info=True;User ID=siberia;Password=siberia.01;MultipleActiveResultSets=True;Trusted_Connection=true;TrustServerCertificate=true;";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
